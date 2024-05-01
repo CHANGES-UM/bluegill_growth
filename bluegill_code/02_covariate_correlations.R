7a1a642f3c387be23710ecbeecf738bc9410c8cd
pjflood <pjfloodecology@gmail.com>
2024-05-01 16:15:09 -0400

    Updated tables

--- bluegill_code/02_covariate_correlations.R
+++ bluegill_code/02_covariate_correlations.R
@@ -20,6 +20,9 @@ covariates <- binded %>%
 var.cor <- rcorr(as.matrix(covariates), type = "pearson")
 var.cor
 
+write.csv(var.cor[[1]], file = "Tables/var_cor.csv")
+
+
 var.cor.long <- var.cor[[1]] %>% as.data.frame() %>% 
   rownames_to_column(var = "var1") %>% 
   pivot_longer(cols = c(LAT_DD:wetlands), names_to = "var2", values_to = "pearsons_r") %>% 
@@ -42,35 +45,35 @@ corrplot(var.cor$r, type = "upper", order = "hclust",
 dev.off()
 
 #correlations among variables included in boosted regression tree models
- brt.covariates <- covariates %>% 
+brt.covariates <- covariates %>% 
    dplyr::select(logcounty, logdepth, DD_mean, doy, logarea, surf_temp_year, wetlands, 
           cpue, forests, agriculture, urban, mean_secchi_m, walleye, pike, perch,
           lmb)
  
  
- brt.var.cor <- rcorr(as.matrix(brt.covariates), type = "pearson")
- brt.var.cor
+brt.var.cor <- rcorr(as.matrix(brt.covariates), type = "pearson")
+brt.var.cor
  
- brt.var.cor.long <- brt.var.cor[[1]] %>% as.data.frame() %>% 
+brt.var.cor.long <- brt.var.cor[[1]] %>% as.data.frame() %>% 
    rownames_to_column(var = "var1") %>% 
    pivot_longer(cols = c(logcounty:lmb), names_to = "var2", values_to = "pearsons_r") %>% 
    arrange(desc(pearsons_r)) %>% 
    filter(var1 != var2)
  
  #set up for plotting
- diag(brt.var.cor$P) <- 0
+diag(brt.var.cor$P) <- 0
  
  #correlation plot
- brt.var.corrplot <- corrplot(brt.var.cor$r, type = "upper", order = "hclust",
+brt.var.corrplot <- corrplot(brt.var.cor$r, type = "upper", order = "hclust",
                           p.mat = brt.var.cor$P, sig.level = 0.05, insig = "blank")
  
- tiff(filename = "Figures/brt_covariates_correlations.tiff", width = 1200, height = 900,
+tiff(filename = "Figures/brt_covariates_correlations.tiff", width = 1200, height = 900,
       units = "px")
  
- corrplot(brt.var.cor$r, type = "upper", order = "hclust",
+corrplot(brt.var.cor$r, type = "upper", order = "hclust",
           p.mat = brt.var.cor$P, sig.level = 0.05, insig = "blank")
  
- dev.off()
+dev.off()
  
 
  
